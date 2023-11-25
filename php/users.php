754bcdf3012f63cd2e23571ca130ec1953970782
unknown <hei.tsiory@gmail.com>
2023-11-25 14:26:30 +0300

    final result

--- php/users.php
+++ php/users.php
@@ -7,7 +7,7 @@ $outgoing_id = $_SESSION['unique_id'];
 $sql = mysqli_query($conn , "SELECT * from users where not unique_id = '{$outgoing_id}'");
 
 $output = "";
-if (mysqli_num_rows($sql) == 1 ){
+if (mysqli_num_rows($sql) == 0 ){
     $output .= "no available users";
 }elseif (mysqli_num_rows($sql) > 0){
 
